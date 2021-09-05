376e6eda9cadde7a4c9e9726588911333ed8f4db
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-09-05 22:07:52 +0700

    Update Ariel.js

--- plugins/Ariel.js
+++ plugins/Ariel.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ariel-sama Kawaii
+Ysup-sama Kakkoi
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /@6288279550172/i
+handler.customPrefix = /@6281284160630/i
 handler.command = new RegExp
 
 module.exports = handler
