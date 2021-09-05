e46354222b913508533c2d83c5c7c4e12975b316
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-09-05 22:06:09 +0700

    Update Ilman.js

--- plugins/Ilman.js
+++ plugins/Ilman.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Elaina-chan Punya Ilman Onii-chan  >///<
+Ryuna-chan Punya Yusup Onii-chan  >///<
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
-handler.customPrefix = /Elaina/i
+handler.customPrefix = /Ryuna/i
 handler.command = new RegExp
 
 module.exports = handler
