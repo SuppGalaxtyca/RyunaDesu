fa7612576ad4d84e7905d7a74ea54de959707210
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-09-16 06:24:30 +0100

    Update register.js

--- plugins/register.js
+++ plugins/register.js
@@ -8,8 +8,8 @@ let handler = async function (m, { text, usedPrefix, command }) {
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
   age = parseInt(age)
-  if (age > 70) throw 'Umur terlalu tua'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 40) throw 'Umur terlalu tua'
+  if (age < 13) throw 'Bayi bisa ngetik sesuai format bjir ._.'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
@@ -17,13 +17,11 @@ let handler = async function (m, { text, usedPrefix, command }) {
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
 Daftar berhasil!
-
 ┌─〔 Info 〕
 ├ Nama: ${name}
 ├ Umur: ${age} tahun
 ├ SN: ${sn}
 └────
-
 SN digunakan untuk unreg, jadi simpan/bintangi pesan ini
 `.trim())
 }
@@ -33,4 +31,3 @@ handler.tags = ['xp']
 handler.command = /^(daftar|reg(ister)?)$/i
 
 module.exports = handler
-
