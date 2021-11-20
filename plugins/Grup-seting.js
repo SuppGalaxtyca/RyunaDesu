ea7e08a65829817fe379017007aed30a4d498c89
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-11-21 00:51:24 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 contoh:
 ${usedPrefix + command} tutup
 ${usedPrefix + command} buka
-	`.trim(), '© SHIRAORI', 'Buka', ',grup 1', 'Tutup', ',grup 0')
+	`.trim(), '© RYUNA', 'Buka', ',grup 1', 'Tutup', ',grup 0')
 		throw false
 	}
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
