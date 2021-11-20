a9f7bf10643078be32e2112bf107a0eca522a0db
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-11-21 00:49:51 +0700

    Update Ilman.js

--- plugins/Ilman.js
+++ plugins/Ilman.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text }) => {
     let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
   conn.reply(m.chat, `
-Ada Yang Bisa Ryuna Bantu?Ryuna Menyukai Semua Member Xyonji.Music>///<
+Ada Yang Bisa Ryuna Bantu?
 `.trim(), m)
     let mentionedJid = [m.sender]
 }
