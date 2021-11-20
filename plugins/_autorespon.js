e1b5415fe33a59ee2c68347ea5ddea4cd3c2b592
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-11-21 01:05:22 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -14,9 +14,9 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'Haiik Shiraori Tidak aktif' : banned ? 'Onii-chan Shiraori mau bilang kamu dibanned' : 'Shiraori Disini',
+                isBanned ? 'Haiik Ryuna Tidak aktif' : banned ? 'Onii-chan Ryuna mau bilang kamu dibanned' : 'Ryuna Disini',
                 '© Shiraori',
-                isBanned ? 'UNBAN' : banned ? 'PEMILIK SHIRAORI' : 'MENU',
+                isBanned ? 'UNBAN' : banned ? 'PEMILIK RYUNA' : 'MENU',
                 isBanned ? '.unban' : banned ? '.owner' : '.?',
                 m.isGroup ? 'BAN' : isBanned ? 'UNBAN' : 'DONASI',
                 m.isGroup ? '.ban' : isBanned ? '.unban' : '.donasi')
@@ -33,8 +33,8 @@ handler.all = async function (m, { isBlocked }) {
 ├ Premium / Rp 5,000
 └────
 
-wa.me/6281351047727
-`.trim(), '© Shiraori', 'PEMILIK SHIRAORI', ',owner', { contextInfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
+wa.me/6281284160630
+`.trim(), '© Shiraori', 'PEMILIK RYUNA', ',owner', { contextInfo: { mentionedJid: [global.owner[0] + '@s.whatsapp.net'] } })
     }
 
     // salam
@@ -64,7 +64,7 @@ wa.me/6281351047727
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | SHIRAORI BOT`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | RYUNA CHAN`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
