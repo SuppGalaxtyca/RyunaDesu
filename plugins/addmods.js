931342c3dc13cd2a2fb6db13ecd406d86490b03a
SuppGalaxtyca <90016600+SuppGalaxtyca@users.noreply.github.com>
2021-11-21 10:04:55 +0700

    Update addmods.js

--- plugins/addmods.js
+++ plugins/addmods.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '') + '@s.whatsapp.net'
     else who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
-    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} dia admin shiraori!`
+    if (json.includes(who.split`@`[0])) throw `${conn.getName(who)} dia admin Ryuna!`
     json.push(`${who.split`@`[0]}`)
     fs.writeFileSync('./src/moderator.json', JSON.stringify(json))
     m.reply(`${conn.getName(who)} telah menjadi admin shiraori!`)
